872612cf4a5fe4afb585b1024885cd10472c4445
juanita <juanitabb2277@gmail.com>
2026-05-01 20:35:37 -0500

    prueba

--- src/main/java/org/example/util/ConexionBD.java
+++ src/main/java/org/example/util/ConexionBD.java
@@ -5,19 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-    private static final String URL = "jdbc:mysql://localhost:3312/JUANITA";
+    private static final String URL = "jdbc:mysql://localhost:3312/prueba";
     private static final String USUARIO = "root";
     private static final String CONTRASENA = "1234";
 
     public static Connection obtenerConexion() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/JUANITA",
-                    "root",
-                    "tu_password"
-            );
+
+            Connection con = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+
+            System.out.println(" Conexion exitosa");
+            return con;
+
         } catch (Exception e) {
+            System.out.println("Error de conexion:");
             e.printStackTrace();
             return null;
         }
